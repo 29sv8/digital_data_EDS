7a5afd64d250e20ce431bda43eb4e315a23f4720
Sanne Vreugdenhil <sanne.vreugdenhil@nioz.nl>
2024-05-13 11:19:08 +0200

    Update file_handling_methods.py
    Addition of extract_zip and files_to_tar_gz

--- file_handling_methods.py
+++ file_handling_methods.py
@@ -203,3 +203,106 @@ def file_from_ftp(
     ftp.quit() # quit the ftp server
     print(f"The file {filename} was succesfully downloaded.")
 # ==============================================================================
+
+
+# ZIP ==========================================================================
+def extract_zip(file):
+    """
+    Description
+        ----
+        This function uses the zipfile module that is included in Python (
+        version 3.10.9) to extact files from a zip. This function extracts
+        the files from the zip and places them into the current working 
+        directory. This function then uses the the build-in os module to 
+        create a new directory and move the files into that new directory.
+        The only thing that it needs to work is the path to the zipfile.
+        ----
+    Parameters
+        ----
+        file
+            >> the path to the zipfile
+        ----
+    Returns
+        ----
+        Creates a folder into the current working directory with the files 
+        from the zip.
+        ----
+    Versioning
+        ----
+        Python: 3.10.9
+        zipfile included in Python version
+        os included in Python version
+        ----
+    """
+    import zipfile # import module for opening zip files
+    import os      # import os to create a new directory to append zipfiles to
+    
+    zip = open(file, "rb")          # open the zipfile 
+    zipShape = zipfile.ZipFile(zip) # read the zipfile
+    
+    files = [] # create a list to append files from inside the zip to
+    
+    for filename in zipShape.namelist(): # obtain filenames 
+        filename = f"{filename}"             # get actual filename
+        out = open(filename, "wb")  
+        out.write(zipShape.read(filename))
+        out.close()
+        files.append(filename)               # append filenames to the list
+    
+    folder_name = filename.split(".")[0] # create dirname from filename
+    os.mkdir(folder_name)                # create a new folder
+    for file in files:
+        os.rename(f"{file}", f"{folder_name}/{file}") # move files to dir
+# ==============================================================================
+
+
+# TAR ==========================================================================
+def files_to_tar_gz(files, tar_name, dir_path=""):
+    """Description
+        ----
+        This function uses the tarfile module that is included in Python (
+        version 3.10.9) to create a tar archive with given files. To do so
+        it uses the w:gz mode for gzipped compression. The names of the files
+        need to be provided in a list, and the to be name of the archive must
+        be provided as well. If files in the current working directory need to 
+        be archived and compressed, a dir_path should not be provided. If files
+        in another directory need to be archived, you do need to provide a path.
+        An alternative for providing a dir_path, is providing the filenames as 
+        a path. (e.g. ["hancock/hancock.dbf", "hancock/hancock.shp", 
+        "hancock/hancock/shx"], note that this way is not officially tested but 
+        should work nonetheless). 
+        Please note that the new compressed archive file is always created in 
+        the current working directory. 
+        ----
+    Parameters
+        ----
+        files
+            >> a list of file names (or paths) that need to be archived and 
+            compressed
+        tar_name
+            >> the name of the new tar.gz file, provide the name without the 
+            file extension!
+        dir_path
+            >> the name(s) of the directory(s) that lead to the files to be 
+            archived are located
+        ----
+    Returns
+        ----
+        Creates a gzipped tar archive in the currnt working directory.
+        ----
+    Versioning
+        ----
+        Python: 3.10.9
+        tarfile included in Python version
+        ----
+    
+    """
+    import tarfile
+    tar = tarfile.open(f"{tar_name}.tar.gz", "w:gz")
+    for file in files:
+        if folder_name != "":
+            file_path = f"{dir_path}/{file}"
+        else:
+            file_path = file
+        tar.add(file_path)
+    tar.close()
